813bc39a61ce92cc5bf40baca417267bc110b060
minseo <gwonm305@gmail.com>
2024-11-24 21:14:34 +0900

    #2 응답처리,예외처리 구현

---
+++ src/main/java/jungmo/server/global/error/ErrorCode.java
@@ -0,0 +1,21 @@
+package jungmo.server.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    // Common
+    INTERNAL_SERVER_ERROR(500, "C001", "internal server error"),
+    INVALID_INPUT_VALUE(400, "C002", "invalid input type"),
+    METHOD_NOT_ALLOWED(405, "C003", "method not allowed"),
+    INVALID_TYPE_VALUE(400, "C004", "invalid type value"),
+    BAD_CREDENTIALS(400, "C005", "bad credentials");
+
+
+    private int status;
+    private final String code;
+    private final String message;
+}
