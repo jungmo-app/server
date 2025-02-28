ad7550477133a4c2954776168c3de5f666183730
minseo <gwonm305@gmail.com>
2025-02-28 15:23:52 +0900

    fix: 헤더 exposed 수정

--- src/main/java/jungmo/server/global/config/CorsConfig.java
+++ src/main/java/jungmo/server/global/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 
 @Configuration
 public class CorsConfig {
@@ -17,6 +19,7 @@ public class CorsConfig {
         configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
         configuration.addAllowedHeader("*"); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 인증 정보 포함 허용
+        configuration.setExposedHeaders(List.of("Set-Cookie"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
